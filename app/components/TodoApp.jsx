3d1ba72b276f7e8d062bcabd115e5ace2add846a
Tim Arsenault <timarsen@gmail.com>
2017-02-15 11:18:40 -0700

    Add test for handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
+
 
 
 var TodoApp = React.createClass({
@@ -11,23 +13,31 @@ var TodoApp = React.createClass({
             searchText: "",
             todos: [
                 {
-                    id: 1,
+                    id: uuid(),
                     text: "Walk the dog"
                 },{
-                    id: 2,
+                    id: uuid(),
                     text: "Save the World from tyranny"
                 },{
-                    id: 3,
+                    id: uuid(),
                     text: "Nap"
                 },{
-                    id: 4,
+                    id: uuid(),
                     text: "Lunch"
                 }
             ]
         };
     },
     handleAddTodo: function (text) {
-        alert('new Todo: ' + text)
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        })
     },
     handleSearch: function (showCompleted, searchText) {
         this.setState({
