82c5bb0ef309ff7dc32ccb500e00841fa5f749d7
Tim Arsenault <timarsen@gmail.com>
2017-02-17 12:48:30 -0700

    Refactor App to work with local storage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -62,6 +62,24 @@ describe('Reducers', () => {
             expect(res[0].completed).toEqual(false);
             expect(res[0].completedAt).toBe(undefined);
         });
+
+        it('should add existing todos', () => {
+            var todos = [{
+                id: 111,
+                text: 'text',
+                completed: false,
+                completedAt: undefined,
+                createdAT: 30030304
+            }];
+            var action = {
+                type: 'ADD_TODOS',
+                todos
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0]).toEqual(todos[0]);
+        });
     });
     
 }); 
